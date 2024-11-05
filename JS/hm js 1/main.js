8b2eb61f831e59afe1242ddaec4cc02595d518ee
user <zabrodskijmihajlo@gmail.com>
2024-11-05 14:59:03 +0200

    js

---
+++ JS/hm js 1/main.js
@@ -0,0 +1,58 @@
+let r = 'hello';
+console.log(r)
+let t = 'owu';
+console.log(t)
+let y = 'com';
+console.log(y)
+let d = 'ua';
+console.log(d)
+let e = 1;
+console.log(e)
+let f = 10;
+console.log(f)
+let g = -999;
+console.log(g)
+let h = 123;
+console.log(h)
+let i = 3.14;
+console.log(i)
+let j = 2.7;
+console.log(j)
+let k = 16;
+console.log(k)
+let l = true;
+console.log(l)
+let m = false;
+console.log(m)
+/*-----------------------------------------------------*/
+
+let firstName =  `Михайло`;
+let middleName =`Вікторович`;
+let lastName =  `Забродський` ;
+
+let msg = lastName+' '+firstName+' '+middleName;
+console.log(msg)
+
+let msg2 = `${lastName} ${firstName} ${middleName}`;
+console.log(msg2)
+
+/*-----------------------------------------------------*/
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof  a)
+console.log(typeof  b)
+console.log(typeof  c)
+/*-----------------------------------------------------*/
+
+let name = prompt("Введіть ваше ім'я:");
+console.log("Ваше ім'я:", name);
+
+let middleNames = prompt("Введіть ваше По-Батькові:");
+console.log("Ваше ім'я:", middleNames);
+
+let year = prompt("Введіть ваш вік:");
+
+console.log("Ваше ім'я:", year);
+
